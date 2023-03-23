c5db768d0c0b50cdd4e1abe386e32b3db8aa62b3
xzq <xzq_xiongzhiqiang@163.com>
2023-03-23 22:35:15 +0800

    init react-pc

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Layout from './pages/layout'
+import Login from './pages/login'
 
 function App () {
   return (
-    <div className="App">
-      this is  react app
-    </div>
+    //路由配置
+    <BrowserRouter>
+      <div className='App'>
+        <Routes>
+          {/* 创建路由path和组件对应关系 */}
+          <Route path='/' element={<Layout />} ></Route>
+          <Route path='/login' element={<Login />} ></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/layout/index.js
@@ -0,0 +1,7 @@
+function Layout () {
+  return (
+    <div>this is Layout</div>
+  )
+}
+
+export default Layout 
